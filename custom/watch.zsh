f7d45ef7539e5b1295f5694ad4974f6c774d8b35
ramen <dave@ramenlabs.com>
2010-09-26 08:52:04 -0700

    Watch for my friends

---
+++ custom/watch.zsh
@@ -0,0 +1,5 @@
+# watch.zsh - Watch for my friends
+
+watch=(notme)                   # watch for everybody but me
+LOGCHECK=300                    # check every 5 min for login/logout activity
+WATCHFMT='%n %a %l from %m at %t.'
