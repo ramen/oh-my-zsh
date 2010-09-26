5881053c137d93811a0c9387c61fd0eaf836fce7
ramen <dave@ramenlabs.com>
2010-09-26 08:51:11 -0700

    Shell command aliases

---
+++ custom/aliases.zsh
@@ -0,0 +1,10 @@
+# aliases.zsh - Shell command aliases
+
+# Don't laugh.
+alias pine='TERM=rxvt alpine'
+
+# Handy shortcut to resume partial scp transfers using rsync.
+alias scpresume='rsync --partial --progress --rsh=ssh'
+
+# As of sudo 1.7.4, $HOME is reset by default. Boo.
+alias sudo='sudo HOME="$HOME"'
