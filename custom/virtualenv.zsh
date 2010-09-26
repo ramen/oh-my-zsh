47e772274102cafae63289e1e93ee10ef9d1abe6
ramen <dave@ramenlabs.com>
2010-09-26 07:26:38 -0700

    Add virtualenv environment (thanks passy)

---
+++ custom/virtualenv.zsh
@@ -0,0 +1,9 @@
+# virtualenvwrapper and pip
+# from mitsuhiko's dotfiles
+if [ `id -u` != '0' ]; then
+  export VIRTUALENV_USE_DISTRIBUTE=1
+  export WORKON_HOME=$HOME/.virtualenvs
+  [ -f /usr/local/bin/virtualenvwrapper.sh ] && source /usr/local/bin/virtualenvwrapper.sh
+  export PIP_VIRTUALENV_BASE=$WORKON_HOME
+  export PIP_RESPECT_VIRTUALENV=true
+fi
